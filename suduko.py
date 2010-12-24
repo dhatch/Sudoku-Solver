99a99e57736f87144c7c110e8d80a77ca92056e3
dhatch387 <dhatch387@gmail.com>
2010-12-24 00:13:44 -0500

    added argparse

--- suduko.py
+++ suduko.py
@@ -1,4 +1,10 @@
+import argparse
+
 def main():
+    parser = argparse.ArgumentParser(description="Solves Suduko Puzzles.")
+    parser.add_argument('filename', type=file, help="name of file to load for suduko puzzle")
+    parser.add_argument("-v, --verbose", help='produce detailed output', action='store_true')
+    args = parser.parse_args()
     
 
 if __name__ == '__main__':
